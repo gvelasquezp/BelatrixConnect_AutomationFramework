170817bc7d54252711252babc9f49f6a057228fb
Nicolas Mori <nicolas.mori@gmail.com>
2016-09-16 11:14:46 -0300

    creacion del framwork sample

---
+++ BelatrixConnect_AutomationDSL/src/main/java/com/belatrix/connect/App.java
@@ -0,0 +1,13 @@
+package com.belatrix.connect;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
